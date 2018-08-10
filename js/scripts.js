5dae6ae3aa9c34f6cdac6c9258786ab8e51faaae
singedphoenix <danielaberle@singedphoenix.com>
2018-08-10 13:59:13 -0700

    code works, jquery stage is complete

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,59 @@ $(document).ready(function() {
     var answer2 = $("select#question2").val();
     var answer3 = $("select#question3").val();
     var answer4 = $("select#question4").val();
-    var answer4 = $("select#question5").val();
+    var answer5 = $("select#question5").val();
 
 
-    if (answer1 === "1") {
+    if (answer1 === "1" && answer2 === "2" && answer3 === "2" && answer4 === "2" && answer5 === "3") {
+      $("#answer-react").show();
+      $('#answer-ruby').hide();
+      $('#answer-csharp').hide();
+      $('#answer-oops').hide();
+    } else if (answer1 === "2" && answer2 === "2" && answer3 === "2" && answer4 === "2" && answer5 === "3") {
+      $("#answer-react").show();
+      $('#answer-ruby').hide();
+      $('#answer-csharp').hide();
+      $('#answer-oops').hide();
+    } else if (answer1 === "2" && answer2 === "2" && answer3 === "2" && answer4 === "2" && answer5 === "1") {
+      $("#answer-react").show();
+      $('#answer-ruby').hide();
+      $('#answer-csharp').hide();
+      $('#answer-oops').hide();
+    } else if (answer1 === "2" && answer2 === "2" && answer3 === "2" && answer4 === "3" && answer5 === "2") {
       $("#answer-ruby").show();
+      $('#answer-react').hide();
+      $('#answer-csharp').hide();
+      $('#answer-oops').hide();
+    } else if (answer1 === "2" && answer2 === "2" && answer3 === "1" && answer4 === "3" && answer5 === "2") {
+      $("#answer-ruby").show();
+      $('#answer-react').hide();
+      $('#answer-csharp').hide();
+      $('#answer-oops').hide();
+    } else if (answer1 === "1" && answer2 === "2" && answer3 === "1" && answer4 === "3" && answer5 === "2") {
+      $("#answer-ruby").show();
+      $('#answer-react').hide();
+      $('#answer-csharp').hide();
+      $('#answer-oops').hide();
+    } else if (answer1 === "1" && answer2 === "1" && answer3 === "1" && answer4 === "1" && answer5 === "1") {
+      $("#answer-csharp").show();
+      $('#answer-react').hide();
+      $('#answer-ruby').hide();
+      $('#answer-oops').hide();
+    } else if (answer1 === "1" && answer2 === "1" && answer3 === "2" && answer4 === "1" && answer5 === "1") {
+      $("#answer-csharp").show();
+      $('#answer-react').hide();
+      $('#answer-ruby').hide();
+      $('#answer-oops').hide();
+    } else if (answer1 === "2" && answer2 === "1" && answer3 === "1" && answer4 === "1" && answer5 === "1") {
+      $("#answer-csharp").show();
+      $('#answer-react').hide();
+      $('#answer-ruby').hide();
+      $('#answer-oops').hide();
+    } else { //In case of empty survey field
+      $("#answer-oops").show();
+      $('#answer-react').hide();
+      $('#answer-ruby').hide();
+      $('#answer-csharp').hide();
     }
   });
 });
